a2cb9470aac1930e81628884f12b7b1953c407ce
EvinPara <96207677+Evin06@users.noreply.github.com>
2024-10-09 14:17:42 +0200

    Update wp-config.php

--- Projet/data/wordpress/wp-config.php
+++ Projet/data/wordpress/wp-config.php
@@ -1,54 +1,96 @@
 <?php
 /**
- * Fichier de configuration de base pour WordPress.
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
  */
 
-// ** Paramètres MySQL ** //
-define('DB_NAME', getenv('WORDPRESS_DB_NAME') ?: 'db_swarm');
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'db_swarm' );
 
-/** Nom utilisateur de la base de données */
-define('DB_USER', getenv('WORDPRESS_DB_USER') ?: 'swarm');
+/** Database username */
+define( 'DB_USER', 'root' );
 
-/** Mot de passe de la base de données */
-define('DB_PASSWORD', file_get_contents('/run/secrets/db_password'));
+/** Database password */
+define( 'DB_PASSWORD', 'root' );
 
-/** Hôte de la base de données MySQL */
-define('DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'db:3306');
+/** Database hostname */
+define( 'DB_HOST', 'db:3306' );
 
-/** Jeu de caractères de la base de données */
-define('DB_CHARSET', 'utf8');
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
 
-/** Type de collation de la base de données */
-define('DB_COLLATE', '');
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
 /**#@+
- * Clés uniques d'authentification et de salage.
- * Remplacez ces valeurs par celles générées par WordPress : https://api.wordpress.org/secret-key/1.1/salt/
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
  */
-define('AUTH_KEY',         '-T(gIfxU,?952-fx&:dKxli;yws7LRi:5v0+m[+0ZtF?ZY{v*kmF:|)B%0]~$m11');
-define('SECURE_AUTH_KEY',  '|a4#6kRTCFG+5]i?[iY^E{+|nQxA!E6||V>EqjIE%-Bk2`j_5~a)X7]( X(9`ssc');
-define('LOGGED_IN_KEY',    'xg^igo|-%AYm>_02$L=7QUl-fOr^<X9#o`(U-vMEX8|oVE8!P.~!_C-nPz_@/),1');
-define('NONCE_KEY',        'sQyA<-^yx`_DZOii5-2{R^WlR+V`.XFP0|LG`][!~uMRm29oy?AvA)v$r~$]J}JV');
-define('AUTH_SALT',        ']L~cDZNP<dEC#Wc6uscEVDSCPH^Aj&.jk-N)24Qy:a,A_z+*EJF!*!:Fv*L|QGKu');
-define('SECURE_AUTH_SALT', '].d|(+y}=:*SV3.Qw`tu Dn3(>3ZN#t#B-ET<}0`B>4Ac8G%YM|`Pq*3=**/%z]/');
-define('LOGGED_IN_SALT',   'GSN6h(fI{d2f.XM+c_:Dxf}<_M`2-~V}KO-]Y$GwBqzE:J${{$R]]a^#o6hzqOJ}');
-define('NONCE_SALT',       'e)Swo+i}++r,7|@43-!LtOD3$H;8~&LVrr?4aJW#[5ZUM/Vy D~@c6Ap?|mTR^O+');
+define( 'AUTH_KEY',         'lpoQ74esp6Gn6;)o=b>Pw=`C`C/259LZp8>bs^r[mX}K$x%^AXs3LiZoC7vQ;%@F' );
+define( 'SECURE_AUTH_KEY',  '2vRZ,94FDv{<[:n;-rsFT(=PQTmvR>|7f?)Af-*IZp^O_L`L;45rEB{W^ljW{kSX' );
+define( 'LOGGED_IN_KEY',    '`_-`Nz8re!!JAFEJzmzX`Bk%)mj%D/J7U~scwET>2~qu/4} 5`)}JpNy6qhuU[8y' );
+define( 'NONCE_KEY',        'W{A$Wo$l`o>r3gBZGPv,}(5>nSOr5RtRN$jsN-bBQb{K%4;)^7pTs.09Mn-F-~fE' );
+define( 'AUTH_SALT',        '!PqP#7,tx@>_91?YZn=7XfLqaFm7JkHt:8kmi^GqzcFe7?MQ u`}@aBVjc)(!Ksh' );
+define( 'SECURE_AUTH_SALT', 'Ce[~hyp9@f]gt)dF9-vQgq<)NrOJw!%:O~T_1KKSDR.VI07)>*r6qP[xb1-9_{ z' );
+define( 'LOGGED_IN_SALT',   'ELj3W|p&:47Y)C.2QW[!`GbM579d]`o=xgEjCzl#;s/{Y,$Ee{hk7cpgBw{0xczP' );
+define( 'NONCE_SALT',       '7?yUNQ=}@EE)g[qQ$%jg=|)K9,?G[LAad&DB4Lma[0JgFFydmrLG}Ng{a7/=34)b' );
 
 /**#@-*/
 
-/** Préfixe des tables dans la base de données */
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
 $table_prefix = 'wp_';
 
-/** Mode de débogage */
-define('WP_DEBUG', false);
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
 
-/** Désactiver l'édition de fichiers dans le tableau de bord */
-define('DISALLOW_FILE_EDIT', true);
+/* Add any custom values between this line and the "stop editing" line. */
 
-/** Spécifier le chemin absolu à la racine de WordPress */
-if (!defined('ABSPATH')) {
-    define('ABSPATH', dirname(__FILE__) . '/');
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-/** Inclure les fichiers WordPress */
-require_once(ABSPATH . 'wp-settings.php');
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
